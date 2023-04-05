823e9b132c601d8b68577ef851a0a3ab53c7d88d
Tatyana58 <tatbir@rambler.ru>
2023-04-05 23:20:27 +0300

    Создали классы, интерфейс  и pom.xml Реализация Сервиса Проверьте ДЗ

--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -1,10 +1,10 @@
-import java.awt.*;
-import java.util.List.*;
+import java.util.List;
 
 public interface EmployeeDAO {
     void addEmployeeTable(Employee employee);
     Employee getEmployeeId(int id);
-   // List<Employee> getAddEmployeeTable();
+    List<Employee> getAddEmployeeTable();
+    // List<Employee> getAddEmployeeTable();
     void updateEmployeeId(int id, Employee employee);
     void deleteEmployeeId(int id);
 }
