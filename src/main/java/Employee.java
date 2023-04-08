ff24e6052d9747e24df802c8d817343d7d31e350
Tatyana58 <tatbir@rambler.ru>
2023-04-08 17:07:02 +0300

    Создали классы, интерфейс  и pom.xml Реализация Сервиса Проверьте ДЗ Проверка реализации методов Проверьте ДЗ. Передалала. Ветка

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,6 +1,5 @@
 import lombok.*;
 import model.City;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -13,86 +12,4 @@ public class Employee {
     private String gender;
     private int age;
     private City city;
-
-//    public Employee(int id, String first_name, String last_name, String gender, int age, City city) {
-//        this.id = id;
-//        this.first_name = first_name;
-//        this.last_name = last_name;
-//        this.gender = gender;
-//        this.age = age;
-//        this.city = city;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirst_name() {
-//        return first_name;
-//    }
-//
-//    public void setFirst_name(String first_name) {
-//        this.first_name = first_name;
-//    }
-//
-//    public String getLast_name() {
-//        return last_name;
-//    }
-//
-//    public void setLast_name(String last_name) {
-//        this.last_name = last_name;
-//    }
-//
-//    public String getGender() {
-//        return gender;
-//    }
-//
-//    public void setGender(String gender) {
-//        this.gender = gender;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public City getCity() {
-//        return city;
-//    }
-//
-//    public void setCity(City city) {
-//        this.city = city;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Employee employee = (Employee) o;
-//        return id == employee.id && age == employee.age && first_name.equals(employee.first_name) && last_name.equals(employee.last_name) && gender.equals(employee.gender) && city.equals(employee.city);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, first_name, last_name, gender, age, city);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Employee{" +
-//                "id=" + id +
-//                ", first_name='" + first_name + '\'' +
-//                ", last_name='" + last_name + '\'' +
-//                ", gender='" + gender + '\'' +
-//                ", age=" + age +
-//                ", city=" + city +
-//                '}';
-//    }
 }
